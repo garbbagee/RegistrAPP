baf26835a7bb992842f0ff88c4065559c15da9a1
garbbagee <romandiazmiguelignacio@gmail.com>
2024-10-28 23:37:39 -0300

    mmm

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'RegistrAPP',
+  appName: 'RegistrApp',
   webDir: 'www'
 };
 
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
+// home.page.ts
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
+  userName: string = 'Estudiante'; // Puedes reemplazar esto con el nombre del usuario real después de la autenticación.
 
-  constructor() {}
+  constructor(private navCtrl: NavController) {}
 
+  ngOnInit() {
+    // Aquí puedes cargar el nombre del usuario si es necesario.
+  }
+
+  onStart() {
+    // Aquí puedes navegar a otra página, por ejemplo, a la página de escaneo de asistencia
+    this.navCtrl.navigateForward('/attendance-scan'); // Cambia '/attendance-scan' al path correspondiente
+  }
 }
