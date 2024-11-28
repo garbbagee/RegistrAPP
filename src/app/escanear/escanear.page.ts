a5dcc53f61eb9374038a7817f95ad06616367e7a
garbbagee <romandiazmiguelignacio@gmail.com>
2024-11-28 02:32:33 -0300

    abre la camara

--- src/app/escanear/escanear.page.ts
+++ src/app/escanear/escanear.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import jsQR from 'jsqr';  // Importar jsQR para leer códigos QR
 
 @Component({
   selector: 'app-escanear',
@@ -7,9 +9,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EscanearPage implements OnInit {
 
+  scannedData: string = ''; // Almacenamos los datos del QR escaneado
+
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  // Método para capturar la imagen con la cámara
+  async scanQRCode() {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      source: CameraSource.Camera,
+      resultType: CameraResultType.Uri,
+    });
+
+    // Verificamos si image.webPath está definido
+    const imageUrl = image.webPath ? image.webPath : '';  // Asignamos un valor vacío si es undefined
+    
+    if (imageUrl) {
+      // Convierte la imagen a base64 y procesa con jsQR
+      const img = new Image();
+      img.src = imageUrl;
+
+      img.onload = () => {
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas.height = img.height;
+        canvas.width = img.width;
+        ctx?.drawImage(img, 0, 0);
+        
+        const imageData = ctx?.getImageData(0, 0, canvas.width, canvas.height);
+        if (imageData) {
+          const qrCode = jsQR(imageData.data, canvas.width, canvas.height);
+          if (qrCode) {
+            this.scannedData = qrCode.data;  // Almacena los datos del QR
+            console.log("QR Escaneado:", this.scannedData);
+          } else {
+            console.log("No se pudo leer el código QR.");
+          }
+        }
+      };
+    } else {
+      console.log("Error: No se pudo obtener la ruta de la imagen.");
+    }
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  firebaseConfig : {
+      apiKey: "AIzaSyAh-M6kY11RH0vCUkJ6tmM2QtN5D8eiWJ8",
+      authDomain: "registrapp-bbadc.firebaseapp.com",
+      projectId: "registrapp-bbadc",
+      storageBucket: "registrapp-bbadc.appspot.com",
+      messagingSenderId: "79991631303",
+      appId: "1:79991631303:web:36badc41ad5a7f53d4bde2"
+    }
+    
+  }
