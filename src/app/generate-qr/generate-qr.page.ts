5d382c7a3443e1de845a53eb5002a89104215729
garbbagee <romandiazmiguelignacio@gmail.com>
2024-12-11 18:07:21 -0300

    GENERA QRRR

--- src/app/generate-qr/generate-qr.page.ts
+++ src/app/generate-qr/generate-qr.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as QRCode from 'qrcode'; // Importa la librería QRCode
 
 @Component({
   selector: 'app-generate-qr',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./generate-qr.page.scss'],
 })
 export class GenerateQrPage implements OnInit {
+  qrData: string = ''; // Variable para almacenar el código QR generado
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  generateQRCode() {
+    // Generar un código QR único para el profesor (puede incluir un ID, URL o información única)
+    const uniqueData = 'asistenciaProfesor123'; // Esta sería la información única para el registro de asistencia
+    QRCode.toDataURL(uniqueData, { width: 256 }, (err, url) => {
+      if (err) {
+        console.error('Error al generar el QR', err);
+      } else {
+        this.qrData = url; // Asignar el QR generado al src de la imagen
+      }
+    });
+  }
 }
